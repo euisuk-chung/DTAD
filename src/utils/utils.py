4fdedcaddeb18533f6ac70a1165aa4dc131cb9e5
Hyeongwon Kang <31944451+hwk0702@users.noreply.github.com>
2021-08-29 15:17:01 +0900

    update

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -13,3 +13,23 @@ def set_seed(random_seed):
     np.random.seed(random_seed)
     random.seed(random_seed)
 
+class EarlyStopping():
+    def __init__(self, patience=0, verbose=0):
+        self._step = 0
+        self._loss = float('inf')
+        self.patience = patience
+        self.verbose = verbose
+
+    def validate(self, loss):
+        if self._loss < loss:
+            self._step += 1
+            if self._step > self.patience:
+                if self.verbose:
+                    print(f'Training process is stopped early....')
+                return True
+        else:
+            self._step = 0
+            self._loss = loss
+
+        return False
+
